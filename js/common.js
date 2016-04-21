f783a3e6293a2af485fde46c0072d5193201d7cf
Atul Bhalerao <atul.bhalerao@weboniselab.com>
2016-04-21 10:09:29 +0530

    gh-pages
    navigation and animation

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.naviGation h4').on('click', function(){
+		//alert();
+		//$('.naviG').addClass('openDoc');
+		//});
+  	$('.naviG').toggle('slide', 'left', 500);
+	});
+});
